9c8e5f89cc6a3c2a49f18ed3b69e5db6a4e0bc1d
Forson666 <51457747+Forson666@users.noreply.github.com>
2020-02-21 13:10:57 -0500

    Add files via upload

---
+++ ejercicio 21.py
@@ -0,0 +1,16 @@
+def dia(x):
+	if x == 1:
+		return "Lunes"
+	elif x == 2:
+		return "Martes"
+	elif x == 3:
+		return "Miercoles"
+	elif x == 4:
+		return "Jueves"
+	elif x == 5:
+		return "Viernes"
+	elif x == 6:
+		return "Sabado"
+	else:
+		return "Domingo"
+	
---
+++ ejercicio 22.py
@@ -0,0 +1,6 @@
+def vocal(x):
+	if x == 'a' or x == 'e' or x == 'i' or x == '0' or x == 'u' or x == 'A' or x == 'E' or x == 'I' or x == 'O' or x == 'U':
+		return x + " si es una vocal"
+	else:
+		return x + " no es una vocal"
+	
